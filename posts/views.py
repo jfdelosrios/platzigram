c34a36d51eb745258da3c011f595821d168e2322
Jorge Fernando De Los Rios <jfdelosrios@hotmail.com>
2021-02-14 16:32:33 -0500

    Finalizado video 9.

--- posts/views.py
+++ posts/views.py
@@ -1,40 +1,41 @@
 """Post views."""
 
 #Django
-from django.http import HttpResponse
+from django.shortcuts import render
 
 # Utilities
 from datetime import datetime
 
 posts =[
         {
-            'name': 'Mont Blac',
-            'user': 'Yésica Cortés',
+            'title': 'Mont Blanc',
+            'user': {
+                'name':'Yésica Cortés',
+                'picture': 'https://picsum.photos/60/60/?image=1027',
+            },
             'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-            'picture': 'https://i.picsum.photos/id/1036/200/200.jpg?hmac=Yb5E0WTltIYlUDPDqT-d0Llaaq0mJnwiCUtxx8RrtVE',
+            'photo': 'https://i.picsum.photos/id/1036/200/200.jpg?hmac=Yb5E0WTltIYlUDPDqT-d0Llaaq0mJnwiCUtxx8RrtVE',
         },
         {
-            'name': 'Via Láctea',
-            'user': 'C. Vander',
+            'title': 'Via Láctea',
+            'user': {
+                'name':'Christian Vander Henst',
+                'picture':'https://picsum.photos/60/60/?image=1005',
+            },
             'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-            'picture': 'https://i.picsum.photos/id/903/200/200.jpg?hmac=lxHKyjlQqAkKyuVGkgUCO_jdWkg3osj3nTuULFHZxH8',
+            'photo': 'https://i.picsum.photos/id/903/200/200.jpg?hmac=lxHKyjlQqAkKyuVGkgUCO_jdWkg3osj3nTuULFHZxH8',
         },
         {
-            'name': 'Nuevo auditorio',
-            'user': 'Thespianartist',
+            'title': 'Nuevo auditorio',
+            'user': {
+                'name':'Uriel (thepianastist)',
+                'picture':'https://picsum.photos/60/60/?image=883',
+            },
             'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-            'picture': 'https://i.picsum.photos/id/1076/200/200.jpg?hmac=KTOq4o7b6rXzwd8kYN0nWrPIeKI97mzxBdWhnn-o-Nc',
+            'photo': 'https://i.picsum.photos/id/1076/200/200.jpg?hmac=KTOq4o7b6rXzwd8kYN0nWrPIeKI97mzxBdWhnn-o-Nc',
         },
     ]
 
 def list_posts(request):
     """List existing posts."""
-    content=[]
-    for post in posts:
-        content.append("""
-        <p><strong>{name}</strong></p>
-        <p><small>{user} - <i>{timestamp}</i></small></p>
-        <figure><img src="{picture}"/></figure>
-        """.format(**post))
-
-    return HttpResponse('<br>'.join(content))
+    return render(request, 'feed.html', {'posts': posts})
